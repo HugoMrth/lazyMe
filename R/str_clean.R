250a3be2398a05a078deb3aa50e73aa5d7adc027
Hugo Marthinet <h.marthinet@gmail.com>
2024-07-08 11:40:42 +0200

    Update str_clean.R

--- R/str_clean.R
+++ R/str_clean.R
@@ -3,11 +3,10 @@ str_clean <- function(x) {
   #### Check Params ####
 
   if(is.null(x)){
-    stop("x manquant")
+    stop("x missing")
   }
 
   #### Code Fonction ####
-
-
-  stri_trans_general(str_to_lower(pander::trim.spaces(x)), id = "Latin-ASCII")
+  res <- stri_trans_general(str_to_lower(pander::trim.spaces(x)), id = "Latin-ASCII")
+  return(res)
 }
